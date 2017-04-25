ccab760ea2196536f935fa0be082bf0e07473be4
Won <wenwenwen888@live.com>
2017-04-25 17:14:09 +0800

    test new branch

--- app/src/main/java/cn/flyexp/backgroundupdate/MainActivity.java
+++ app/src/main/java/cn/flyexp/backgroundupdate/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
         intent = new Intent(MainActivity.this, DownloadService.class);
 
-        //开启服务
+        //开启服务 --- 新分支
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
